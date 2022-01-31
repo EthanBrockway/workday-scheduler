bd8037ef5ae28a033c65047d971a8c3d926d9b8c
Ethan Brockway <ebrockway01@gmail.com>
2022-01-30 21:39:19 -0600

    added save button functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,10 @@
-var currentDate = moment().format("ddd-MMM-Do-YYYY");
-
+var currentDate = moment().format("ddd, MMM Do, YYYY");
 $("#currentDay").html(currentDate);
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  });
+});
