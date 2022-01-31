2ba3fe5e5f1a990aee35bf0ec5bdefdd248b4ab8
Ethan Brockway <ebrockway01@gmail.com>
2022-01-30 22:35:35 -0600

    added time block if statements for past present and future tasks to do

--- Develop/script.js
+++ Develop/script.js
@@ -7,4 +7,25 @@ $(document).ready(function () {
 
     localStorage.setItem(time, text);
   });
+  var timeTrack = function () {
+    var timeNow = moment().hour();
+
+    $(".time-block").each(function () {
+      var timeBlock = parseInt$(this).attr("id").split("hour")[1];
+
+      if (timeBlock > timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } else if (timeBlock === timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
+    });
+  };
 });
